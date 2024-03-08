9ee19703c7695eef50c81819ee1dfdae17c195b1
bryc <1408749+bryc@users.noreply.github.com>
2024-03-08 13:37:38 -0500

    Add files via upload

---
+++ UGL/wiiww.js
@@ -0,0 +1,30 @@
+gdata.wiiww = {
+    A1: [ // Good
+    "And Yet It Moves"
+    ],
+    A2: [
+    "Alien Crush Returns"
+    ],
+    A3: [
+    ],
+    B1: [ // Decent
+    ],
+    B2: [
+    ],
+    B3: [
+    ],
+    C1: [ // Acceptable
+    ],
+    C2: [
+    ],
+    C3: [
+    ],
+    D: [ // Dubious or Skeptical
+    ],
+    E: [ // Bad
+    ],
+    F: [ // Terrible
+    ],
+    X: [ // Skip (typically sports games that are too similar to other games)
+    ],
+};
