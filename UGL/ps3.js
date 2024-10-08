36e963dfeaa64b2a01d768980037c101065b8b7c
bryc <1408749+bryc@users.noreply.github.com>
2024-10-08 03:16:21 -0400

    Add files via upload

---
+++ UGL/ps3.js
@@ -0,0 +1,30 @@
+gdata.ps3 = {
+    A1: [ // Good
+    "Gran Turismo 5 - XL Edition",
+    "Gran Turismo 6"
+    ],
+    A2: [
+    ],
+    A3: [
+    ],
+    B1: [ // Decent
+    ],
+    B2: [
+    ],
+    B3: [
+    ],
+    C1: [ // Acceptable
+    ],
+    C2: [
+    ],
+    C3: [
+    ],
+    D: [ // Dubious or Skeptical
+    ],
+    E: [ // Bad
+    ],
+    F: [ // Terrible
+    ],
+    X: [ // Skip (typically sports games that are too similar to other games)
+    ],
+};
